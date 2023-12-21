9ed32f3777c70c009d25da136e9e210a2b08a9af
kimgura <oli999@naver.com>
2023-12-21 11:41:09 +0900

    Boot03에 Thymeleaf 예제 추가함

--- Boot03_Thymeleaf/src/test/java/com/example/boot03/Boot03ThymeleafApplicationTests.java
+++ Boot03_Thymeleaf/src/test/java/com/example/boot03/Boot03ThymeleafApplicationTests.java
@@ -1,11 +1,26 @@
 package com.example.boot03;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.boot03.controller.HomeController;
 
 @SpringBootTest
 class Boot03ThymeleafApplicationTests {
-
+	
+	@Autowired
+	HomeController homeController;
+	
+	@DisplayName("home 엔트 포인트 테스트")
+	@Test
+	void homeTest() {
+		assert homeController != null;
+	}
+	
 	@Test
 	void contextLoads() {
 	}
