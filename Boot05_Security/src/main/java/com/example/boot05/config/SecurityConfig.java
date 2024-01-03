2bb671df0358bf5fa5cd0ffc1edb377480e5782a
kimgura <oli999@naver.com>
2024-01-03 12:28:28 +0900

    Boot05 에 Spring Security 기능 완성함

--- Boot05_Security/src/main/java/com/example/boot05/config/SecurityConfig.java
+++ Boot05_Security/src/main/java/com/example/boot05/config/SecurityConfig.java
@@ -18,10 +18,13 @@ public class SecurityConfig {
 	@Bean //메소드에서 리턴되는 SecurityFilterChain 을 bean 으로 만들어준다.
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
 		//화이트 리스트를 미리 배열에 넣어두기
-		String[] whiteList= {"/", "/play", "/notice", "/user/loginform"};
+		String[] whiteList= {"/", "/play", "/notice", "/user/loginform", 
+				"/user/login_fail", "/user/expired"};
 		
 		//메소드의 매개변수에 HttpSecurity 의 참조값이 전달되는데 해당 객체를 이용해서 설정을 한다음
-		httpSecurity.authorizeHttpRequests(config -> 
+		httpSecurity
+		.csrf(csrf->csrf.disable())
+		.authorizeHttpRequests(config -> 
 			config
 				.requestMatchers(whiteList).permitAll() //whiteList 요청은 로그인과 상관없이 모두 허용
 				.requestMatchers("/admin/**").hasRole("ADMIN")
@@ -30,11 +33,31 @@ public class SecurityConfig {
 		)
 		.formLogin(config -> 
 			config
+				//인증을 거치지 않은 사용자를 리다일렉트 시킬 경로 설정 
+				.loginPage("/user/required_loginform")
+				//로그인 처리를 할때 요청될 url 설정 ( spring security 가 login 처리를 대신 해준다)
 				.loginProcessingUrl("/user/login")
+				//로그인 처리를 대신 하려면 어떤 파라미터명으로 username 과 password 가 넘어오는지 알려주기 
 				.usernameParameter("userName") 
 				.passwordParameter("password")
-				.successForwardUrl("/user/login_success")
+				.successHandler(new AuthSuccessHandler())//로그인 성공 핸들러 등록
+				.failureForwardUrl("/user/login_fail") //로그인 실패시 forward 될 url 설정
+				.permitAll() //위에 명시한 모든 요청경로를 로그인 없이 요청할수 있도록 설정 
+		)
+		.logout(config ->
+			config
+				.logoutUrl("/user/logout")//Spring Security 가 자동으로 로그아웃 처리 해줄 경로 설정
+				.logoutSuccessUrl("/")//로그 아웃 이후에 리다일렉트 시킬 경로 설정
 				.permitAll()
+		)
+		.exceptionHandling(config ->
+			//403 forbidden 인 경우 forward 이동 시킬 경로 설정 
+			config.accessDeniedPage("/user/denied")
+		)
+		.sessionManagement(config ->
+			config
+				.maximumSessions(1) //최대 허용 세션 갯수
+				.expiredUrl("/user/expired") //허용 세션 갯수가 넘어서 로그인 해제된 경우 리다일렉트 이동시킬 경로
 		);
 		//설정된 정보대로 SecurityFilterChain 객체를 만들어서 반환한다 
 		return httpSecurity.build();
