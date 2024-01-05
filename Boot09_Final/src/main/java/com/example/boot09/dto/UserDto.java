a155422c97ecbd8837b7e36319df609e946d7602
kimgura <oli999@naver.com>
2024-01-05 17:18:55 +0900

    Boot09 에 Spring Security 설정 추가함

---
+++ Boot09_Final/src/main/java/com/example/boot09/dto/UserDto.java
@@ -0,0 +1,45 @@
+package com.example.boot09.dto;
+
+public class UserDto {
+	//숫자로된 아이디는 PK 
+	private int id;
+	//사용자명 은 중복된 데이터가 들어가지 않도록 UNIQUE KEY 를 설정해야 한다
+	private String userName;
+	private String password;
+	private String email;
+	//Authority 정보를 저장할 칼럼 ROLE_XXX 형식이다. 
+	private String role;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
