322c51a187a02d446deb184dcefacbb6c4fbbae4
kimgura <oli999@naver.com>
2024-01-12 17:44:45 +0900

    Boot09 에 자료실 기능 구현할 준비중...

--- Boot09_Final/src/main/resources/table.sql
+++ Boot09_Final/src/main/resources/table.sql
@@ -38,6 +38,19 @@ FROM
 WHERE num=5
 
 
+-- 업로드된 파일의 정보를 저장할 테이블
+CREATE TABLE board_file(
+	num NUMBER PRIMARY KEY,
+	writer VARCHAR2(100) NOT NULL,
+	title VARCHAR2(100) NOT NULL,
+	orgFileName VARCHAR2(100) NOT NULL, -- 원본 파일명
+	saveFileName VARCHAR2(100) NOT NULL, -- 서버에 실제로 저장된 파일명
+	fileSize NUMBER NOT NULL, -- 파일의 크기 
+	regdate DATE
+);
+
+CREATE SEQUENCE board_file_seq; 
+
 
 
 
